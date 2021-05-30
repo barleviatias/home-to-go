cc3cbeca358f1ae27f9e109799a7cd4430ed6f5f
ofek vaknin <vakofek10@gmail.com>
2021-05-30 14:06:24 +0300

    add stays db

--- src/cmps/dashboard/MyPlaces.jsx
+++ src/cmps/dashboard/MyPlaces.jsx
@@ -1,75 +1,4 @@
-import { Component } from 'react';
-import { stayService } from '../../services/stay-service.js'
-
-// export function MyPlaces() {
-
-//     const { stays } = this.props
-//     console.log(stays);
-//     if (!stays) return <h3>Loading....</h3>
-//     return (
-//         <main>
-
-//             <h1>My Stays</h1>
-//             <table>
-//                 <thead>
-//                     <tr>
-//                         <td>Id</td>
-//                         <td>Name</td>
-//                         <td>Precie per night</td>
-//                         <td>Address</td>
-//                         <td>Action</td>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     {stays.map(stay => {
-//                         return (
-//                             <tr key={stay._id}>
-//                                 <td>{stay._id}</td>
-//                                 <td>{stay.name}</td>
-//                                 <td>{stay.price}</td>
-//                                 <td>{stay.loc.address}</td>
-//                                 <td>
-//                                     <button >Edit</button>
-//                                     <button onClick={() => this.onRrmoveStays(stay._id)}>Remove</button>
-//                                 </td>
-//                             </tr>
-//                         )
-//                     })}
-//                 </tbody>
-//             </table>
-//         </main>
-//     )
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import {Link} from 'react-router-dom'
 
 
 export function MyPlaces({ stays, removeStay, toggleMsgModal ,onSelectedEditStay }) {
@@ -104,8 +33,8 @@ export function MyPlaces({ stays, removeStay, toggleMsgModal ,onSelectedEditStay
                     {stays.map(stay => {
                         return (
                             <tr key={stay._id}>
-                                <td>{stay._id}</td>
-                                <td>{stay.name}</td>
+                                <td><Link to={`/stay/${stay._id}`}>{stay._id}</Link></td>
+                                <td><Link to={`/stay/${stay._id}`}>{stay.name}</Link></td>
                                 <td>{stay.price}</td>
                                 <td>{stay.loc.address}</td>
                                 <td>
