8a7b59b3fb33bafa89554b70b07abded22d80cda
ofek vaknin <vakofek10@gmail.com>
2021-06-01 20:46:57 +0300

    add host orders to dash

--- src/App.jsx
+++ src/App.jsx
@@ -219,23 +219,6 @@ class _App extends Component {
         </Switch>
         {/* <Footer /> */}
 
-<<<<<<< HEAD
-    return (
-      <Router>
-        <Header trip={trip} addTrip={addTrip} modalType={modalType} onSearch={this.onSearch} loggedInUser={loggedInUser} logout={logout} openDynamicModal={this.openDynamicModal} closeDynamicModal={this.closeDynamicModal} setModalContent={this.setModalContent} />
-        <Switch>
-          <Route path='/login' component={LoginSignup} />
-          <Route path='/orders' render={(props) => (<Orders {...props} loadOrders={loadOrders} orders={orders} loggedInUser={loggedInUser} removeOrder={removeOrder} toggleMsgModal={this.toggleMsgModal} />)} />
-          <Route path='/host/:userId' render={(props) => (<Dashboard {...props} loggedInUser={loggedInUser} updateUser={updateUser} toggleMsgModal={this.toggleMsgModal} loadOrders={loadOrders} orders={orders} />)} />
-          <Route path='/host' render={(props) => (<BecomeHost {...props} loggedInUser={loggedInUser} />)} />
-          <Route path='/wishlist' render={(props) => (<Wishlist {...props} stays={stays} loadWishlist={loadWishlist} loggedInUser={loggedInUser} />)} />
-          <Route path='/stay/:stayId' render={(props) => (<StayDetails {...props} onSearch={this.onSearch} loggedInUser={loggedInUser} toggleMsgModal={this.toggleMsgModal} openDynamicModal={this.openDynamicModal} modalType={modalType} updateUser={updateUser} setModalContent={this.setModalContent} />)} />
-          <Route path='/explore' render={(props) => (<Explore {...props} trip={trip} loadStays={loadStays} stays={stays} updateUser={updateUser} loggedInUser={loggedInUser} />)} />
-          <Route path='/user' render={(props) => (<UserDetails {...props} updateUser={updateUser} />)} />
-          <Route path='/' render={(props) => (<Home {...props} onSearch={this.onSearch} stays={stays} loggedInUser={loggedInUser} loadStays={loadStays} />)} />
-        </Switch>
-        {/* <Footer /> */}
-=======
 				<DynamicModal
           openDynamicModal={this.openDynamicModal}
           modalPosition={dynamicModal.modalPosition}
@@ -243,7 +226,6 @@ class _App extends Component {
         >
           {dynamicModal.modalContent}
         </DynamicModal>
->>>>>>> fe3bf6180d64bcd23f48d1871c621aa6c1406a81
 
         <UserMsg>
           <section className={`user-msg ${isUserMsg && 'on'} `}>
